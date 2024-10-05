2fa39cfeb78f04c4820d8e8c61e7602a070e10e4
Calamytryx <ton.agustin09@gmail.com>
2024-10-06 00:29:33 +0800

    Update env.php

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -1,7 +1,5 @@
 <?php
 
-include 'pw.php';
-$pw_var = pw();
 if (!defined('APP_NAME'))                       define('APP_NAME', 'CoECSA Thesis');
 if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'LPU-C CoECSA');
 if (!defined('APP_OWNER'))                      define('APP_OWNER', '120ms');
@@ -17,6 +15,6 @@ if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
 
 if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
 if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'ton.agustin09@gmail.com');
-if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', $pw_var);
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', '');
 if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
 if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
