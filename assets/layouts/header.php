cbed1b858ccc99fe9a25841a9c76950f3c24abc7
Calamytryx <ton.agustin09@gmail.com>
2024-10-07 18:40:57 +0800

    Update header.php

--- assets/layouts/header.php
+++ assets/layouts/header.php
@@ -29,7 +29,7 @@ check_remember_me();
     <link rel="icon" type="image/png" href="../assets/images/favicon.png">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
