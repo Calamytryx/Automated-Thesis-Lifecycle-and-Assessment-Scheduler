e5c1c614a03b1a45d9fa5224d1f8fdc62ec4df29
Calamytryx <ton.agustin09@gmail.com>
2024-10-07 18:37:55 +0800

    Update header.php

--- assets/layouts/header.php
+++ assets/layouts/header.php
@@ -28,9 +28,12 @@ check_remember_me();
     <title><?php echo TITLE . ' | ' . APP_NAME; ?></title>
     <link rel="icon" type="image/png" href="../assets/images/favicon.png">
 
-    <link rel="stylesheet" href="../assets/vendor/bootstrap-4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/vendor/fontawesome-5.12.0/css/all.min.css">
- 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
     <!-- Custom styles -->
     <link rel="stylesheet" href="../assets/css/app.css">
     <link rel="stylesheet" href="custom.css" >
