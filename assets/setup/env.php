6eff0fdb7d930f71d47fce78c240454c8e4c90a4
Calamytryx <ton.agustin09@gmail.com>
2024-10-07 19:40:13 +0800

    Update env.php

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -11,7 +11,7 @@ if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'coecsa_th
 if (!defined('DB_HOST'))                        define('DB_HOST','127.0.0.1');
 if (!defined('DB_USERNAME'))                    define('DB_USERNAME','root');
 if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
-if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
+if (!defined('DB_PORT'))                        define('DB_PORT' ,'3306');
 
 if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
 if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'ton.agustin09@gmail.com');
