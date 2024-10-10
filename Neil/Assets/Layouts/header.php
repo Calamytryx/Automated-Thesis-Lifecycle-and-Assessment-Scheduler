a167323cbeb388aed78f9eb1cd5ef2164ba39304
NeilV19 <143886462+NeilV19@users.noreply.github.com>
2024-10-11 00:09:43 +0800

    The folder you asked for?
    idk man, took me ages cus my custom css conflicted with the format. at least now I know

---
+++ Neil/Assets/Layouts/header.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+
+require '../assets/setup/env.php';
+require '../assets/setup/db.inc.php';
+require '../assets/includes/auth_functions.php';
+require '../assets/includes/security_functions.php';
+
+if (isset($_SESSION['auth']))
+    $_SESSION['expire'] = ALLOWED_INACTIVITY_TIME;
+
+generate_csrf_token();
+check_remember_me();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="<?php echo APP_DESCRIPTION;  ?>">
+    <meta name="author" content="<?php echo APP_OWNER;  ?>">
+
+    <title><?php echo TITLE . ' | ' . APP_NAME; ?></title>
+    <link rel="icon" type="image/png" href="../assets/images/favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="../assets/css/app.css">
+    <link rel="stylesheet" href="../login/custom.css" >
+    <link rel="stylesheet" href="../register/custom.css" >
+
+</head>
+
+<body>
+
+    <?php require 'navbar.php'; ?>
---
+++ Neil/Login/index.php
@@ -0,0 +1,77 @@
+<?php
+define('TITLE', "Login");
+include '../assets/layouts/header.php';
+check_logged_out();
+?>
+
+<div class="container" id="log-reg-container">
+    <div class="row g-0">
+        <div class="col-md-6 img-container">
+            <img src="../assets/images/login-bg.png" alt="Building">
+        </div>
+        <div class="col-md-6 form-container">
+            <div class="logo">
+                <span class="text-dark">A</span><span class="text-primary">AA</span>
+            </div>
+            <form class="form-auth" action="includes/login.inc.php" method="post">
+
+                <?php insert_csrf_token(); ?>
+
+                <div class="text-center mb-3">
+                    <small class="text-success font-weight-bold">
+                        <?php
+                            if (isset($_SESSION['STATUS']['loginstatus']))
+                                echo $_SESSION['STATUS']['loginstatus'];
+                        ?>
+                    </small>
+                </div>
+
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username or email</label>
+                    <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+                    <sub class="text-danger">
+                        <?php
+                            if (isset($_SESSION['ERRORS']['nouser']))
+                                echo $_SESSION['ERRORS']['nouser'];
+                        ?>
+                    </sub>
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+                    <sub class="text-danger">
+                        <?php
+                            if (isset($_SESSION['ERRORS']['wrongpassword']))
+                                echo $_SESSION['ERRORS']['wrongpassword'];
+                        ?>
+                    </sub>
+                </div>
+
+                <div class="mb-3 text-end">
+                    <a href="../reset-password/" class="text-decoration-none">Forgot password?</a>
+                </div>
+
+                <button class="btn btn-primary w-100" type="submit" value="loginsubmit" name="loginsubmit">Login</button>
+
+                <div class="col-auto my-1 mb-4">
+                    <div class="custom-control custom-checkbox mr-sm-2">
+                        <input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
+                        <label class="custom-control-label" for="rememberme">Remember me</label>
+                    </div>
+                </div>
+
+                <p class="mt-4 mb-3 text-muted text-center">
+                    <a href="" target="_blank"><?php echo APP_NAME; ?></a> |
+                    <a href="" target="_blank"><?php echo APP_ORGANIZATION; ?></a>
+                </p>
+            </form>
+        </div>
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+<?php
+include '../assets/layouts/footer.php';
+?>
