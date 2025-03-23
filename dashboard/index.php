116c00b8ae6b953b5203903e988f4b526b069344
Calamytryx <ton.agustin09@gmail.com>
2025-03-23 22:11:30 +0800

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -363,10 +363,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <a class="nav-link my-1" id="env-variables-tab" data-bs-toggle="pill" href="#env-variables" role="tab" aria-controls="env-variables" aria-selected="false">
                                 <i class="fas fa-cog me-2"></i>Content Management
                             </a>
-                            <a class="nav-link my-1" href="../files" role="tab">
+                            <a class="nav-link my-1" href="../files" target="_blank" role="tab">
                                 <i class="fas fa-folder-open me-2"></i>Files
                             </a>
-                            <a class="nav-link my-1" href="../../phpmyadmin" role="tab">
+                            <a class="nav-link my-1" href="../../phpmyadmin" target="_blank" role="tab">
                                 <i class="fas fa-database me-2"></i>DataBase
                             </a>
                         </div>
