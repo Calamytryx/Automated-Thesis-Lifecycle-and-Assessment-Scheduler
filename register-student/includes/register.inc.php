40c9648afa0430c616b59eb38990ee306493e485
Winston Agustin <winston.agustin@lpunetwork.edu.ph>
2025-08-07 07:33:15 +0800

    Update email domain for student and teacher registration
    Changed the default email domain for student registration to '@lpunetwork.edu.ph' and for teacher registration to '@lpu.edu.ph' to reflect updated institutional requirements.

--- register-student/includes/register.inc.php
+++ register-student/includes/register.inc.php
@@ -47,7 +47,7 @@ if (isset($_POST['signupsubmit'])) {
     }
     
     $username = input_filter($_POST['username']);
-    $email = input_filter($_POST['email'] . "@gmail.com");
+    $email = input_filter($_POST['email'] . "@lpunetwork.edu.ph");
     $password = input_filter($_POST['password']);
     $passwordRepeat  = input_filter($_POST['confirmpassword']);
     $headline = input_filter($_POST['headline']);
