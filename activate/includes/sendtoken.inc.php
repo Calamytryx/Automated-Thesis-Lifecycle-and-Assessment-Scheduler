8f1ae289c6c2989e7c7f01bb2e1e4096a7d4f49f
Calamytryx <91600814+Calamytryx@users.noreply.github.com>
2025-08-25 18:15:02 +0800

    reskin forgot password

---
+++ activate/includes/sendtoken.inc.php
@@ -0,0 +1,134 @@
+<?php
+
+session_start();
+
+require '../../assets/includes/security_functions.php';
+require '../../assets/includes/auth_functions.php';
+check_logged_out();
+
+require '../../assets/setup/env.php';
+require '../../assets/setup/db.inc.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../assets/vendor/PHPMailer/src/Exception.php';
+require '../../assets/vendor/PHPMailer/src/PHPMailer.php';
+require '../../assets/vendor/PHPMailer/src/SMTP.php';
+
+if (isset($_POST['resentsend'])) {
+
+    /*
+    * -------------------------------------------------------------------------------
+    *   Securing against Header Injection
+    * -------------------------------------------------------------------------------
+    */
+
+    foreach($_POST as $key => $value){
+        $_POST[$key] = _cleaninjections(trim($value));
+    }
+
+    /*
+    * -------------------------------------------------------------------------------
+    *   Verifying CSRF token
+    * -------------------------------------------------------------------------------
+    */
+
+    if (!verify_csrf_token()){
+        $_SESSION['STATUS']['resentsend'] = 'Request could not be validated';
+        header("Location: ../");
+        exit();
+    }
+
+    $selector = bin2hex(random_bytes(8));
+    $token = random_bytes(32);
+    $url = "http://atlas.iceiy.com/reset-password/?selector=" . $selector . "&validator=" . bin2hex($token);
+    $expires = date('Y-m-d H:i:s', strtotime('+1 hour'));
+
+    $email = $_POST['email'];
+
+    // First, delete any existing tokens for this email
+    $sql = "DELETE FROM auth_tokens WHERE user_email=? AND auth_type='password_reset'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+
+    // Now insert the new token
+    $sql = "INSERT INTO auth_tokens (user_email, auth_type, selector, token, expires_at) 
+            VALUES (?, 'password_reset', ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+    $stmt->execute([$email, $selector, $hashedToken, $expires]);
+
+    $to = $email;
+    $subject = 'Reset Your Password';
+    
+    /*
+    * -------------------------------------------------------------------------------
+    *   Using email template
+    * -------------------------------------------------------------------------------
+    */
+
+    $mail_variables = array();
+
+    $mail_variables['APP_NAME'] = APP_NAME;
+    $mail_variables['email'] = $email;
+    $mail_variables['url'] = $url;
+
+    $message = file_get_contents("./template_passwordresetemail.php");
+
+    foreach($mail_variables as $key => $value) {
+        
+        $message = str_replace('{{ '.$key.' }}', $value, $message);
+    }
+
+    $mail = new PHPMailer(true);
+
+    try {
+
+        $mail->isSMTP();
+        $mail->Host = MAIL_HOST;
+        $mail->SMTPAuth = true;
+        $mail->Username = MAIL_USERNAME;
+        $mail->Password = MAIL_PASSWORD;
+        $mail->SMTPSecure = MAIL_ENCRYPTION;
+        $mail->Port = MAIL_PORT;
+
+        $mail->setFrom(MAIL_USERNAME, APP_NAME);
+        $mail->addAddress($to, APP_NAME);
+
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+
+        $mail->send();
+        
+        $_SESSION['STATUS']['resentsend'] = 'Password reset email sent';
+        header("Location: ../");
+        exit();
+    } 
+    catch (Exception $e) {
+
+        // for public use
+        $_SESSION['STATUS']['mailstatus'] = 'message could not be sent, try again later';
+
+        // for development use
+        // $_SESSION['STATUS']['mailstatus'] = 'message could not be sent. ERROR: ' . $mail->ErrorInfo;
+
+        header("Location: ../");
+        exit();
+    }
+
+    /*
+    * ------------------------------------------------------------
+    *   Script Endpoint 
+    * ------------------------------------------------------------
+    */
+    $_SESSION['STATUS']['resentsend'] = 'Password reset email sent';
+    header("Location: ../");
+    exit();
+}
+else {
+
+    header("Location: ../");
+    exit();
+}
---
+++ activate/index.php
@@ -0,0 +1,103 @@
+<?php
+
+define('TITLE', "Login");
+include '../assets/layouts/header.php';
+check_logged_out();
+
+?>
+
+
+
+<div class="reset-wrapper position-relative min-vh-100">
+    <div class="container position-relative">
+        <div class="row min-vh-100 align-items-center justify-content-center justify-content-lg">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4 py-4">
+                <div class="card shadow-lg p-4 reset-card" style="max-width: 480px; width: 100%;">
+                    <div class="card-body">
+                        <div class="text-center mb-4">
+                            <img src="../assets/images/logo_full_lightbg.png" alt="Logo" class="img-fluid" style="width: 200px; height: auto;">
+                        </div>
+                        <?php if (isset($_GET['selector']) && isset($_GET['validator'])) { ?>
+                            <form class="form-auth" action="includes/activate.inc.php" method="post">
+                                <?php
+                                    insert_csrf_token();
+                                    $selector = $_GET['selector'];
+                                    $validator = $_GET['validator'];
+                                ?>
+                                <input type="hidden" name="selector" value="<?php echo $selector; ?>">
+                                <input type="hidden" name="validator" value="<?php echo $validator; ?>">
+                                <h6 class="h5 mb-3 font-weight-normal text-muted text-center">Activate Email</h6>
+                                <div class="text-center mb-3">
+                                    <small class="text-success font-weight-bold">
+                                        <?php
+                                            if (isset($_SESSION['STATUS']['resetsubmit']))
+                                                echo $_SESSION['STATUS']['resetsubmit'];
+                                        ?>
+                                    </small>
+                                </div>
+                                <div class="text-center mb-3">
+                                    <sub class="text-danger">
+                                        <?php
+                                            if (isset($_SESSION['ERRORS']['passworderror']))
+                                                echo $_SESSION['ERRORS']['passworderror'];
+                                        ?>
+                                    </sub>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" id="newpassword" name="newpassword" class="form-control" placeholder="New Password" autocomplete="new-password">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input type="password" id="confirmpassword" name="confirmpassword" class="form-control" placeholder="Confirm Password" autocomplete="new-password">
+                                </div>
+                                <button class="btn btn-primary w-100" type="submit" value="resetsubmit" name="resetsubmit">
+                                    Reset Password
+                                </button>
+                            </form>
+                        <?php } else { ?>
+                            <form class="form-auth" action="includes/sendtoken.inc.php" method="post">
+                                <?php insert_csrf_token(); ?>
+                                <h6 class="h5 mb-3 font-weight-normal text-muted text-center">Activate Email</h6>
+                                <div class="text-center mb-3">
+                                    <small class="text-success font-weight-bold">
+                                        <?php
+                                            if (isset($_SESSION['STATUS']['resentsend']))
+                                                echo $_SESSION['STATUS']['resentsend'];
+                                        ?>
+                                    </small>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <label for="email" class="sr-only">Email</label>
+                                    <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
+                                    <sub class="text-danger">
+                                        <?php
+                                            if (isset($_SESSION['ERRORS']['emailerror']))
+                                                echo $_SESSION['ERRORS']['emailerror'];
+                                        ?>
+                                    </sub>
+                                </div>
+                                <button class="btn btn-primary w-100" type="submit" value="resentsend" name="resentsend">
+                                    Send Activation Link
+                                </button>
+                                <!-- <p class="mt-4 mb-3 text-muted text-center">
+                                    <a href="https://github.com/msaad1999/PHP-Login-System" target="_blank">
+                                        Login System
+                                    </a> | 
+                                    <a href="https://github.com/msaad1999/PHP-Login-System/blob/master/LICENSE" target="_blank">
+                                        MIT License
+                                    </a>
+                                </p> -->
+                            </form>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+
+include '../assets/layouts/footer.php'
+
+?>
