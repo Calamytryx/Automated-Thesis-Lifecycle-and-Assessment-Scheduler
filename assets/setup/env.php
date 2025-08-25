64c8c54939c05a1e4167445d37b2ed382fddc452
Calamytryx <91600814+Calamytryx@users.noreply.github.com>
2025-08-25 13:43:54 +0800

    fixed wrong address

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -8,8 +8,8 @@ if (!defined('APP_NAME'))           define('APP_NAME', getEnvVariable('APP_NAME'
 if (!defined('APP_ORGANIZATION'))   define('APP_ORGANIZATION', getEnvVariable('APP_ORGANIZATION') ?? 'LPU-C CoECSA');
 if (!defined('APP_OWNER'))          define('APP_OWNER', getEnvVariable('APP_OWNER') ?? '120ms');
 if (!defined('APP_DESCRIPTION'))    define('APP_DESCRIPTION', getEnvVariable('APP_DESCRIPTION') ?? 'Advanced Thesis Logistics and AI System for LPU');
-if (!defined('APP_LOGO_NAVBAR'))   define('APP_LOGO_NAVBAR', getEnvVariable('APP_LOGO_NAVBAR') ?? '../assets/images/logo_full_lightbg.png');
-if (!defined('APP_LOGO_FOOTER'))  define('APP_LOGO_FOOTER', getEnvVariable('APP_LOGO_FOOTER') ?? '../assets/images/logowhite.png');
+if (!defined('APP_LOGO_NAVBAR'))   define('APP_LOGO_NAVBAR', getEnvVariable('APP_LOGO_NAVBAR') ?? '../images/logo_full_lightbg.png');
+if (!defined('APP_LOGO_FOOTER'))  define('APP_LOGO_FOOTER', getEnvVariable('APP_LOGO_FOOTER') ?? '../images/logowhite.png');
 
 if (!defined('ALLOWED_INACTIVITY_TIME')) define('ALLOWED_INACTIVITY_TIME', time() + (int)(getEnvVariable('ALLOWED_INACTIVITY_TIME') ?? 1*60*60));
 
